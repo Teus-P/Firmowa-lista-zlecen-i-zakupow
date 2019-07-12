93b74034afdfa2e33eb1a07d60da323a846ab503
Jakub Pitera <pitjakub@gmail.com>
2019-07-12 23:17:38 +0200

    Dodanie plików projektu

--- src/main/java/pl/app/App.java
+++ src/main/java/pl/app/App.java
@@ -5,9 +5,11 @@ package pl.app;
 
 public class App {
 
+    public String getGreeting() {
+        return "Hello world.";
+    }
 
     public static void main(String[] args) {
-
-        System.out.println("chuj XD DDDDDDD");
+        System.out.println(new App().getGreeting());
     }
 }
