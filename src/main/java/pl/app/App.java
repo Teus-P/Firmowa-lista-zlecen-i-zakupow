d0952b2ea5112523ceb1bc62943b2f0872d2525a
Jakub Pitera <pitjakub@gmail.com>
2019-08-01 16:30:49 +0200

    Dodanie plików projektu

--- src/main/java/pl/app/App.java
+++ src/main/java/pl/app/App.java
@@ -3,13 +3,25 @@
  */
 package pl.app;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import java.io.IOException;
+
+public class App extends Application {
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.show();
     }
 }
---
+++ src/main/java/pl/app/controllers/MainController.java
@@ -0,0 +1,4 @@
+package pl.app.controllers;
+
+public class MainController {
+}
